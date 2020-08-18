b89e9d2c552074106653e4dbec9b8a9a8d04fedb
hidran@gmail.com <hidran@gmail.com>
2020-08-18 02:11:44 +0200

    adds Myfirstscript a variables projects

---
+++ MyFirstScript/Assets/Scripts/MySecondScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySecondScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My first script");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
