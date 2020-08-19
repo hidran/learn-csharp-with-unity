4401c57a25c1a3966425bddc865bfe03bfd6b3e6
hidran@gmail.com <hidran@gmail.com>
2020-08-20 00:43:55 +0200

    What is a class and inheritance

---
+++ 006-classes/Assets/Scripts/Car.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public string make = "FIAT";
+    public string model = "500";
+    // public string color = "Red";
+    public Color color = Color.red;
+    public int hp = 150;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("About to start my " + " " + make + " " + model);
+            StartCar();
+        }
+        
+           
+        
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("About to stop my " + " " + make + " " + model);
+            StopCar();
+        }
+    }
+    void StartCar()
+    {
+        Debug.Log("Brrrrrrrrrrrrrrummmmmmmmmmm");
+    }
+    void StopCar()
+    {
+        Debug.Log("The car was stopped");
+    }
+
+}
