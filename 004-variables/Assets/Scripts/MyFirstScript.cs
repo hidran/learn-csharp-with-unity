214e95374fa18edadb1bdcbaefa88a16dba559ed
hidran@gmail.com <hidran@gmail.com>
2020-08-19 23:15:15 +0200

    adds variables and methods projects

---
+++ 004-variables/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    // public, private, protected
+
+    public string  playerName = "Juan Gabriel";
+     public int playerScore = 58;
+    private int maxScore = 120;
+     int playerAge = 28;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(playScore);
+        Debug.Log(playerName);
+        Debug.Log(playerScore + 12);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ 005-methods/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    // public, private, protected
+
+    public string  playerName = "Juan Gabriel";
+     public int playerScore = 58;
+    private int maxScore = 120;
+     int playerAge = 28;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(playScore);
+        //Debug.Log(playerName);
+        //Debug.Log(playerScore + 12);
+        //BirthDay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.B))
+            BirthDay();
+        
+    }
+    // int, string, bool
+   void BirthDay()
+    {
+        Debug.Log("Age before birthday " + playerAge);
+        Debug.Log(playerAge + 1);
+    }
+
+}
