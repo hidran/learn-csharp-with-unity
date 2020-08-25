83fef15c37f2e3105d119576d5a12498776fdc6c
hidran@gmail.com <hidran@gmail.com>
2020-08-26 01:09:24 +0200

    10-public and private properties

--- 006-classes/Assets/Scripts/Car.cs
+++ 006-classes/Assets/Scripts/Car.cs
@@ -2,46 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Car : MonoBehaviour
-{
+public class Car : MonoBehaviour {
     public string make = "FIAT";
     public string model = "500";
     // public string color = "Red";
     public Color color = Color.red;
     public int hp = 150;
+    private operatingSystems = ['ANDROID', 'IOS'];
+    private int numberOfPistons = 3;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Start");
+    void Start () {
+        Debug.Log ("Start");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            Debug.Log("About to start my " + " " + make + " " + model);
-            StartCar();
-        }
-        
-           
-        
+    void Update () {
 
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Debug.Log("About to stop my " + " " + make + " " + model);
-            StopCar();
+if(numberOfPistons > 3){
+
+}
+
+        if (Input.GetKeyDown (KeyCode.C)) {
+            Debug.Log ("About to start my " + " " + make + " " + model);
+            StartCar ();
+        }
+
+        if (Input.GetKeyDown (KeyCode.S)) {
+            Debug.Log ("About to stop my " + " " + make + " " + model);
+            StopCar ();
         }
     }
-    void StartCar()
-    {
-        Debug.Log("Brrrrrrrrrrrrrrummmmmmmmmmm");
+    void StartCar () {
+        Debug.Log ("Brrrrrrrrrrrrrrummmmmmmmmmm");
     }
-    void StopCar()
-    {
-        Debug.Log("The car was stopped");
+    void StopCar () {
+        Debug.Log ("The car was stopped");
     }
 
 }
