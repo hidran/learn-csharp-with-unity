2b860e71d62c4c1f3854871af7392bd8edcf52fa
hidran@gmail.com <hidran@gmail.com>
2020-09-04 01:11:30 +0200

    add lesson 12: built-in types

---
+++ 012-builtin-types/Assets/Builtin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Built-in data types
+public class Builtin : MonoBehaviour
+{
+    private string myName = "Hidran";
+    private bool isSmoker = false;
+
+    public sbyte age = 127;
+    public double price ;
+    public char leftKey = 'A';
+    /*
+     * whole number types : sbyte, byte, ushort, short, int, uint, long, ulong, double
+     * decimal numbers : float, double, decimal
+     */
+    void Start()
+    {
+
+        price = 1/3d; // 0.3333
+        Debug.Log(price.ToString("0.000000000"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
