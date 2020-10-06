40d828208c14b5fb9402ae8cbf700c3d28e1aef0
hidran@gmail.com <hidran@gmail.com>
2020-10-07 01:33:03 +0200

    016. methods

---
+++ 016-methods/Assets/scripts/Methods.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Methods : MonoBehaviour
+{
+     void Awake()
+    {
+        int number = 16;
+
+        IsItEven(number);
+        number = 17;
+        IsItEven(number);
+        IsItEven(23);
+        HelloBeforeStartingTheGame();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Class just started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   void IsItEven( int num)
+    {
+        if (num % 2 == 0)
+        {
+            Debug.Log("Number " + num + " is Even");
+        }
+        else
+        {
+            Debug.Log("Number " + num + "  is Odd");
+        }
+    }
+   void HelloBeforeStartingTheGame()
+    {
+        
+    }
+}
